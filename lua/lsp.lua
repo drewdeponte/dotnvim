5d5dcafd1fe2b7ddfd6b4a96984db354a57724d0
Drew De Ponte <cyphactor@gmail.com>
2022-11-04 23:20:05 -0400

    Switch to default_capabilities
    Switch to default_capabilities because update_capabilities is
    deprecated.
    
    ps-id: 24bcb50d-c87e-4595-b181-a8e394f3436c

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -4,13 +4,14 @@ local nvim_lsp = require('lspconfig')
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+
   local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
   -- Enable completion triggered by <c-x><c-o>
   buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
   -- Mappings.
-  local opts = { noremap=true, silent=true }
+  local opts = { noremap = true, silent = true }
 
   -- See `:help vim.lsp.*` for documentation on any of the below functions
   buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
@@ -31,58 +32,59 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
   buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 
--- nnoremap <silent> gd    <cmd>lua vim.lsp.buf.definition()<CR>
--- nnoremap <silent> gi    <cmd>lua vim.lsp.buf.implementation()<CR>
--- nnoremap <silent> gD    <cmd>lua vim.lsp.buf.declaration()<CR>
--- nnoremap <silent> gT    <cmd>lua vim.lsp.buf.type_definition()<CR>
--- nnoremap <silent> gr    <cmd>lua vim.lsp.buf.references()<CR>
--- nnoremap <silent> gs    <cmd>lua vim.lsp.buf.document_symbol()<CR>
--- nnoremap <silent> gS    <cmd>lua vim.lsp.buf.workspace_symbol()<CR>
--- nnoremap <silent> K     <cmd>lua vim.lsp.buf.hover()<CR>
--- nnoremap <silent> <c-k> <cmd>lua vim.lsp.buf.signature_help()<CR>
+  -- nnoremap <silent> gd    <cmd>lua vim.lsp.buf.definition()<CR>
+  -- nnoremap <silent> gi    <cmd>lua vim.lsp.buf.implementation()<CR>
+  -- nnoremap <silent> gD    <cmd>lua vim.lsp.buf.declaration()<CR>
+  -- nnoremap <silent> gT    <cmd>lua vim.lsp.buf.type_definition()<CR>
+  -- nnoremap <silent> gr    <cmd>lua vim.lsp.buf.references()<CR>
+  -- nnoremap <silent> gs    <cmd>lua vim.lsp.buf.document_symbol()<CR>
+  -- nnoremap <silent> gS    <cmd>lua vim.lsp.buf.workspace_symbol()<CR>
+  -- nnoremap <silent> K     <cmd>lua vim.lsp.buf.hover()<CR>
+  -- nnoremap <silent> <c-k> <cmd>lua vim.lsp.buf.signature_help()<CR>
 end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'tsserver', 'clojure_lsp', 'sourcekit', 'pyright', 'vimls', 'dockerls', 'terraformls', 'graphql', 'dartls', 'sumneko_lua' }
+local servers = { 'tsserver', 'clojure_lsp', 'sourcekit', 'pyright', 'vimls', 'dockerls', 'terraformls', 'graphql',
+  'dartls', 'sumneko_lua' }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
     flags = {
       debounce_text_changes = 150,
     },
-    capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+    capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
   }
 end
 
 local rust_opts = {
-    tools = { -- rust-tools options
-        autoSetHints = true,
-        -- hover_with_actions = true,
-        inlay_hints = {
-            show_parameter_hints = false,
-            parameter_hints_prefix = "",
-            other_hints_prefix = "",
-        },
+  tools = { -- rust-tools options
+    autoSetHints = true,
+    -- hover_with_actions = true,
+    inlay_hints = {
+      show_parameter_hints = false,
+      parameter_hints_prefix = "",
+      other_hints_prefix = "",
     },
+  },
 
-    -- all the opts to send to nvim-lspconfig
-    -- these override the defaults set by rust-tools.nvim
-    -- see https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#rust_analyzer
-    server = {
-        -- on_attach is a callback called when the language server attachs to the buffer
-        on_attach = on_attach,
-        settings = {
-            -- to enable rust-analyzer settings visit:
-            -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
-            ["rust-analyzer"] = {
-                -- enable clippy on save
-                checkOnSave = {
-                    command = "clippy"
-                },
-            }
-        }
-    },
+  -- all the opts to send to nvim-lspconfig
+  -- these override the defaults set by rust-tools.nvim
+  -- see https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#rust_analyzer
+  server = {
+    -- on_attach is a callback called when the language server attachs to the buffer
+    on_attach = on_attach,
+    settings = {
+      -- to enable rust-analyzer settings visit:
+      -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+      ["rust-analyzer"] = {
+        -- enable clippy on save
+        checkOnSave = {
+          command = "clippy"
+        },
+      }
+    }
+  },
 }
 
 require('rust-tools').setup(rust_opts)
@@ -94,41 +96,41 @@ nvim_lsp.ccls.setup {
       threads = 0;
     };
     clang = {
-      excludeArgs = { "-frounding-math"} ;
+      excludeArgs = { "-frounding-math" };
     };
   }
 }
 
 -- Configure JavaScript/TypeScript, etc.
 local function eslint_config_exists()
-	local eslintrc = vim.fn.glob(".eslintrc*", 0, 1)
+  local eslintrc = vim.fn.glob(".eslintrc*", 0, 1)
 
-	if not vim.tbl_isempty(eslintrc) then
-		return true
-	end
+  if not vim.tbl_isempty(eslintrc) then
+    return true
+  end
 
-	if vim.fn.filereadable("package.json") then
-		if vim.fn.json_decode(vim.fn.readfile("package.json"))["eslintConfig"] then
-			return true
-		end
-	end
+  if vim.fn.filereadable("package.json") then
+    if vim.fn.json_decode(vim.fn.readfile("package.json"))["eslintConfig"] then
+      return true
+    end
+  end
 
-	return false
+  return false
 end
 
 local lspconfig = require('lspconfig')
 
 local eslint = {
-	lintCommand = "eslint_d -f unix --stdin --stdin-filename ${INPUT}",
-	lintStdin = true,
-	lintFormats = {"%f:%l:%c: %m"},
-	lintIgnoreExitCode = true,
-	formatCommand = "eslint_d --fix-to-stdout --stdin --stdin-filename=${INPUT}",
-	formatStdin = true
+  lintCommand = "eslint_d -f unix --stdin --stdin-filename ${INPUT}",
+  lintStdin = true,
+  lintFormats = { "%f:%l:%c: %m" },
+  lintIgnoreExitCode = true,
+  formatCommand = "eslint_d --fix-to-stdout --stdin --stdin-filename=${INPUT}",
+  formatStdin = true
 }
 
 local function set_lsp_config(client)
-	if client.resolved_capabilities.document_formatting then
-		vim.cmd [[autocmd! BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 300)]]
-	end
+  if client.resolved_capabilities.document_formatting then
+    vim.cmd [[autocmd! BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 300)]]
+  end
 end
