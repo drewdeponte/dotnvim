507ec91b85ae5f2ad722e6cf654173605983acd1
Drew De Ponte <cyphactor@gmail.com>
2023-03-30 11:33:56 -0400

    Add ensure_installed grammers to tree sitter conf
    Add ensure_installed grammers to tree sitter conf so that when I launch
    neovim it will make sure that those grammers are installed. This just
    makes it easier when I setup my neovim setup on a new machine as I don't
    have to remember to install each of these grammers manually.
    
    <!-- ps-id: f545f2ad-abbc-428d-b8f2-ff41c0e0d55f -->

--- lua/treesitter.lua
+++ lua/treesitter.lua
@@ -3,6 +3,7 @@
 -- ----------------------------------------------
 local ts = require('nvim-treesitter.configs')
 ts.setup {
+  ensure_installed = { "c", "lua", "luadoc", "markdown", "markdown_inline", "rust", "toml", "sql", "swift", "yaml", "terraform", "typescript", "vim", "help", "query", "bash", "css", "scss", "dart", "diff", "dockerfile", "git_rebase", "gitattributes", "gitignore", "graphql", "html", "javascript", "jsdoc", "json" },
   highlight = { enable = true },
   indent = { enable = true },
 }
