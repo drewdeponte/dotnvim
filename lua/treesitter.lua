fe63933500081a9ebeb9ce02cee57f95c8c6020d
Drew De Ponte <cyphactor@gmail.com>
2023-11-29 16:55:47 -0500

    Update treesitter ensure installeds
    Update treesitter ensure installeds so that it matches the latest
    updates for the ones I care about.
    
    <!-- ps-id: 29d6b3e7-0fa8-4721-b658-2b46561883fd -->

--- lua/treesitter.lua
+++ lua/treesitter.lua
@@ -3,7 +3,7 @@
 -- ----------------------------------------------
 local ts = require('nvim-treesitter.configs')
 ts.setup {
-  ensure_installed = { "c", "ruby", "lua", "luadoc", "markdown", "markdown_inline", "rust", "toml", "kotlin", "sql", "swift", "yaml", "terraform", "typescript", "vim", "help", "query", "bash", "css", "scss", "dart", "diff", "dockerfile", "git_rebase", "gitattributes", "gitignore", "graphql", "html", "javascript", "jsdoc", "json" },
+  ensure_installed = { "ruby", "lua", "luadoc", "markdown", "markdown_inline", "rust", "toml", "kotlin", "sql", "swift", "yaml", "terraform", "typescript", "vim", "query", "bash", "css", "scss", "dart", "diff", "dockerfile", "git_rebase", "gitattributes", "gitignore", "graphql", "html", "javascript", "jsdoc", "json", "c" },
   highlight = { enable = true },
   indent = { enable = true },
 }
