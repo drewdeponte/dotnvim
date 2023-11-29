c9d04a3a84b7cc7687363aefdacb65ab94eb4496
Drew De Ponte <cyphactor@gmail.com>
2023-11-29 16:35:44 -0500

    Change the copilot priority in completions
    Change the copilot priority in completions so that the other types of
    completions come first. I did this because copilot doesn't seem to be
    very good at providing that useful of completions.
    
    <!-- ps-id: 1309c0c4-6458-44c5-a892-ab1eac479560 -->

--- lua/completion.lua
+++ lua/completion.lua
@@ -26,7 +26,7 @@ cmp.setup({
     }),
   },
   sources = {
-    { name = 'copilot',  group_index = 2 },
+    { name = 'copilot',  group_index = 3 },
     { name = 'nvim_lua', group_index = 2 },
     { name = 'nvim_lsp', group_index = 2 },
     { name = 'luasnip',  group_index = 2 },
