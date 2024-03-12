4db0ddd59e726d37053e3776af73e980ac424c55
Drew De Ponte <cyphactor@gmail.com>
2024-03-12 09:38:45 -0400

    Set haskell tab behavior
    <!-- ps-id: 2d31c1f3-2857-4ba3-9ffe-82454935ae4e -->

--- init.lua
+++ init.lua
@@ -154,12 +154,12 @@ vim.api.nvim_create_autocmd('FileType', {
 	command = 'setlocal wrap linebreak nolist noexpandtab tabstop=4 shiftwidth=4 softtabstop=0 nosmarttab'
 })
 vim.api.nvim_create_autocmd('FileType', {
-	pattern = { 'tf', 'ts', 'html', 'nroff', 'typescriptreact', 'dart', 'zsh' },
+	pattern = { 'tf', 'ts', 'html', 'nroff', 'typescriptreact', 'dart', 'zsh', 'typescript', 'javascript', 'htmldjango', 'json', 'haskell' },
 	group = file_type_group,
-	command = 'setlocal expandtab tabstop=4 shiftwidth=2 softtabstop=0 smarttab'
+	command = 'setlocal expandtab tabstop=2 shiftwidth=2 softtabstop=0 smarttab'
 })
 vim.api.nvim_create_autocmd('FileType', {
-	pattern = { 'javascript', 'htmldjango', 'rust' },
+	pattern = { 'rust' },
 	group = file_type_group,
 	command = 'setlocal expandtab tabstop=4 shiftwidth=4 softtabstop=0 smarttab'
 })
