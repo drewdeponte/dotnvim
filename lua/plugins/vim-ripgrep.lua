917f9fe5ed8c7b79da51ad037eafde794cd07400
Drew De Ponte <andrew.deponte@ondiem.com>
2025-01-14 12:49:53 -0500

    Add keymaps for doing commen greps

--- lua/plugins/vim-ripgrep.lua
+++ lua/plugins/vim-ripgrep.lua
@@ -1,3 +1,28 @@
 return {
-	'jremmen/vim-ripgrep'
+	'jremmen/vim-ripgrep',
+	config = function()
+		vim.keymap.set("n", "<leader>gar", function()
+				local word = vim.fn.expand("<cword>")
+				if word and word ~= "" then
+					local cmd = string.format("Rg '\\b%s\\b'", word)
+					vim.cmd(cmd)
+				else
+					print("No word under cursor to search!")
+				end
+			end,
+			{ noremap = true, silent = true }
+		)
+
+		vim.keymap.set("n", "<leader>gr", function()
+				local word = vim.fn.expand("<cword>")
+				if word and word ~= "" then
+					local cmd = string.format("Rg '\\b%s\\b' --glob \"!*_spec.rb\"", word)
+					vim.cmd(cmd)
+				else
+					print("No word under cursor to search!")
+				end
+			end,
+			{ noremap = true, silent = true }
+		)
+	end
 }
