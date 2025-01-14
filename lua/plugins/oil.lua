ac701370d67f2b591d5b34255166036f8ca94f3b
Drew De Ponte <andrew.deponte@ondiem.com>
2025-01-14 13:13:09 -0500

    Add oil for file management in nvim

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,18 @@
+return {
+	'stevearc/oil.nvim',
+	---@module 'oil'
+	---@type oil.SetupOpts
+	opts = {},
+	-- Optional dependencies
+	-- dependencies = { { "echasnovski/mini.icons", opts = {} } },
+	dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+	config = function()
+		require("oil").setup({
+			view_options = {
+				show_hidden = true,
+			},
+		})
+
+		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+	end,
+}
