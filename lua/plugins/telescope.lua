f70034686ed5d59d06287e0632083cb30d714555
Drew De Ponte <andrew.deponte@ondiem.com>
2025-01-14 12:51:54 -0500

    Setup more telescope keybindings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,8 +9,13 @@ return {
 		-- Fuzzy Finder Configuration
 		-- ----------------------------------------------
 		vim.keymap.set('n', '<leader>f', '<cmd>lua require(\'telescope.builtin\').find_files()<cr>')
+		vim.keymap.set('n', '<leader>cs', '<cmd>lua require(\'telescope.builtin\').current_buffer_tags()<cr>')
+		vim.keymap.set('n', '<leader>ct', '<cmd>lua require(\'telescope.builtin\').tags()<cr>')
 		vim.keymap.set('n', '<leader>b', '<cmd>lua require(\'telescope.builtin\').buffers()<cr>')
 		vim.keymap.set('n', '<leader>g', '<cmd>lua require(\'telescope.builtin\').live_grep()<cr>')
+		vim.keymap.set('n', '<leader>j', '<cmd>lua require(\'telescope.builtin\').jumplist()<cr>')
+		vim.keymap.set('n', '<leader>l', '<cmd>lua require(\'telescope.builtin\').loclist()<cr>')
+		vim.keymap.set('n', '<leader>k', '<cmd>lua require(\'telescope.builtin\').keymaps()<cr>')
 		vim.keymap.set('n', '<leader>h', '<cmd>lua require(\'telescope.builtin\').help_tags()<cr>')
 	end
 }
