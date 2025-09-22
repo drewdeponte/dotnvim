86b087afa4a237b46dba3f8b2969c081f57e66b9
Drew De Ponte <cyphactor@gmail.com>
2025-09-22 09:48:57 -0400

    Add treesitter text objects plugins for better nav
    Add treesitter text objects plugins for better nav so that I can jump to
    text objects in the language based on an actual AST rather than just
    based on regex matching.
    
    As part of this I also updated all the plugins.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,8 @@ return {
 	'nvim-treesitter/nvim-treesitter',
 	dependencies = {
 		'nvim-treesitter/playground',
-		'nvim-treesitter/nvim-treesitter-context'
+		'nvim-treesitter/nvim-treesitter-textobjects',
+		-- 'nvim-treesitter/nvim-treesitter-context'
 	},
 	version = false,
 	build = ':TSUpdate',
@@ -13,6 +14,28 @@ return {
 			ensure_installed = { "c", "lua", "luadoc", "vim", "vimdoc", "query", "ruby", "markdown", "markdown_inline", "rust", "toml", "kotlin", "sql", "yaml", "terraform", "typescript", "javascript", "jsdoc", "json", "query", "bash", "css", "scss", "dart", "diff", "dockerfile", "git_rebase", "gitattributes", "gitignore", "graphql", "html", "elixir", "heex" },
 			highlight = { enable = true },
 			indent = { enable = true, disable = { "dart" } },
+			textobjects = {
+				move = {
+					enable = true,
+					-- set_jumps = true, -- record jumps in jumplist
+					goto_next_start = {
+					  ["]m"] = { query = "@function.outer", desc = "Next function/method start" },
+					  ["]]"] = { query = "@class.outer", desc = "Next class start" },
+					},
+					goto_next_end = {
+					  ["]M"] = { query = "@function.outer", desc = "Next function/method end" },
+					  ["]["] = { query = "@class.outer", desc = "Next class end" },
+					},
+					goto_previous_start = {
+					  ["[m"] = { query = "@function.outer", desc = "Prev function/method start" },
+					  ["[["] = { query = "@class.outer", desc = "Prev class start" },
+					},
+					goto_previous_end = {
+					  ["[M"] = { query = "@function.outer", desc = "Prev function/method end" },
+					  ["[]"] = { query = "@class.outer", desc = "Prev class end" },
+					},
+				}
+			}
 		})
 	end
 }
