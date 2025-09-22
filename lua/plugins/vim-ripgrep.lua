a1eec5afd3ffa95ed69945733c526be73eeb79b7
Drew De Ponte <cyphactor@gmail.com>
2025-09-22 09:50:34 -0400

    Add my vim-ripgrep key cmd commands to history
    Add my vim-ripgrep key cmd commands to history so that I can simply use
    up arrow in the command history to rerun one of them. This is useful as
    I often get back a list and am working through that list eliminating
    references.

--- lua/plugins/vim-ripgrep.lua
+++ lua/plugins/vim-ripgrep.lua
@@ -5,6 +5,7 @@ return {
 				local word = vim.fn.expand("<cword>")
 				if word and word ~= "" then
 					local cmd = string.format("Rg '\\b%s\\b'", word)
+					vim.fn.histadd("cmd", cmd)
 					vim.cmd(cmd)
 				else
 					print("No word under cursor to search!")
@@ -17,6 +18,7 @@ return {
 				local word = vim.fn.expand("<cword>")
 				if word and word ~= "" then
 					local cmd = string.format("Rg '\\b%s\\b' --glob \"!*_spec.rb\"", word)
+					vim.fn.histadd("cmd", cmd)
 					vim.cmd(cmd)
 				else
 					print("No word under cursor to search!")
